62db806502c6de9b47a94a6ea6ba56a0036a2033
zhangjiali <charliejiali@hotmail.com>
2022-02-22 17:41:46 +0800

    :sparkles: feat: 新增 unplugin-vue-components, unplugin-auto-import，按需引入element plus

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import '@/styles/index.scss'
 
 import router from './router'
@@ -16,5 +13,4 @@ createApp(App)
   .component('svg-icon', svgIcon)
   .use(store)
   .use(router)
-  .use(ElementPlus, { size: 'small', locale: zhCn })
   .mount('#app')
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,28 @@
 <template>
-  <div>home</div>
+  <div class="test-container">
+    <el-button>home</el-button>
+    <div>mock: {{ mockData }}</div><br>
+    <div>online: {{ onlineData }}</div>
+    <el-calendar />
+  </div>
 </template>
 
 <script setup lang="ts">
+import { reactive, toRefs } from 'vue'
 import { getList } from '@/api/home'
+import { getTestList } from '@/api/test'
 
-getList({})
+const dataObj = reactive({
+  mockData: null,
+  onlineData: null
+})
+
+getList({}).then(({ data }) => {
+  dataObj.mockData = data
+})
+getTestList({}).then(({ data }) => {
+  dataObj.onlineData = data
+})
+
+const { mockData, onlineData } = toRefs(dataObj)
 </script>
-
-<style scoped>
-
-</style>
