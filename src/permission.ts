8cc5295bc5150c198c49adb77fe7fa8ea25508fc
zhangjiali <charliejiali@hotmail.com>
2023-02-02 15:46:39 +0800

    :zap: perf(/permission.ts): vuex migration to pinia

--- src/permission.ts
+++ src/permission.ts
@@ -1,10 +1,11 @@
 import router from './router'
-import store from './store'
 import NProgress from 'nprogress' // progress bar
 import 'nprogress/nprogress.css' // progress bar style
 import { getToken } from '@/utils/auth' // get token from cookie
 import getPageTitle from '@/utils/get-page-title'
 import { RouterRowTy } from '~/router'
+import { useUserStore } from '@/stores/user'
+import { usePermissionStore } from '@/stores/permission'
 
 NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
@@ -26,29 +27,30 @@ router.beforeEach(async(to: any, from, next: any) => {
       next({ path: '/' })
       NProgress.done()
     } else {
-      // const pages = store.getters.pages
-      const hasRoles = store.getters.roles && store.getters.roles.length > 0
+      const userStore = useUserStore()
+      const hasRoles = userStore.roles && userStore.roles.length > 0
       if (hasRoles) {
         next()
       } else {
         try {
           // get user info
           // note: roles must be a object array! such as: ['admin'] or ,['developer', 'editor']
-          const { roles } = await store.dispatch('user/getInfo')
+          const { roles } = await userStore.getInfo()
 
           // generate accessible routes map based on roles
-          const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
+          const permissionStore = usePermissionStore()
+          const accessRoutes = await permissionStore.generateRoutes(roles)
 
           // dynamically add accessible routes
           const removeRoutes: any[] = []
           accessRoutes.forEach((route: RouterRowTy) => {
             removeRoutes.push(router.addRoute(route))
           })
-          await store.dispatch('permission/setRemoveRoutes', removeRoutes)
+          await permissionStore.setRemoveRoutes(removeRoutes)
           // set the replace: true, so the navigation will not leave a history record
           next({ ...to, replace: true })
         } catch (error) {
-          await store.dispatch('user/resetToken')
+          await userStore.resetToken()
           ElMessage.error(error as Error || 'Has Error')
           next(`/login?redirect=${to.path}`)
           NProgress.done()
